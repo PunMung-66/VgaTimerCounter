a51cd443987caa2879daf0bbb0c6c3d827d67572
Punnawat <punnawat.mungkalarungsi@gmail.com>
2024-12-01 17:51:26 +0700

    finish minus button

--- imports/VGA Text Generation/top.v
+++ imports/VGA Text Generation/top.v
@@ -22,7 +22,7 @@ module top(
     reg [255:0] data_raw_reg;
 
     // Debounce signals
-    wire btnR_debounced;
+    wire btnR_debounced, btnL_debounced;
 
     // Initial block for data_raw_reg
     initial begin
@@ -37,16 +37,29 @@ module top(
         .pb_out(btnR_debounced)
     );
 
+    debounce_better_version debounce_inst2(
+        .clk(clk),
+        .pb_1(btnL),
+        .pb_out(btnL_debounced)
+    );
+
     // Detect btnR press (rising edge)
     reg btnR_state, btnR_pressed;
+    reg btnL_state, btnL_pressed;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             btnR_state <= 0;
             btnR_pressed <= 0;
+
+            btnL_state <= 0;
+            btnL_pressed <= 0;
         end else begin
             btnR_pressed <= btnR_debounced && !btnR_state; // Detect rising edge
             btnR_state <= btnR_debounced; // Update state
+
+            btnL_pressed <= btnL_debounced && !btnL_state; // Detect rising edge
+            btnL_state <= btnL_debounced; // Update state
         end
     end
 
@@ -121,104 +134,88 @@ module top(
                 data_raw_reg[15:0] = data_raw_reg[15:0] + 1;
             end
 
-            // // Original code
+        end
+        else if (btnL_pressed) begin
+            if (sw[0] == 1 && sw[4] == 1) begin
+                if (data_raw_reg[255:240] == 0) data_raw_reg[255:240] = 16'd0000;
+                else data_raw_reg[255:240] = data_raw_reg[255:240] - 1;
 
-            // if (sw[0] == 1 && sw[4] == 1) begin
-            //     if (add_button) data_raw_reg[255:240] = data_raw_reg[255:240] + 1;
-            //     else if ((data_raw_reg[255:240] - 1) == 0) data_raw_reg[255:240] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[255:240] = data_raw_reg[255:240] - 1;
-            // end
+            end
 
-            // if (sw[0] == 1 && sw[5] == 1) begin
-            //     if (add_button) data_raw_reg[239:224] = data_raw_reg[239:224] + 1;
-            //     else if ((data_raw_reg[239:224] - 1) == 0) data_raw_reg[239:224] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[239:224] = data_raw_reg[239:224] - 1;
-            // end
+            if (sw[0] == 1 && sw[5] == 1) begin
+                if (data_raw_reg[239:224] == 0) data_raw_reg[239:224] = 16'd0000;
+                else data_raw_reg[239:224] = data_raw_reg[239:224] - 1;
+            end
 
-            // if (sw[0] == 1 && sw[6] == 1) begin
-            //     if (add_button) data_raw_reg[223:208] = data_raw_reg[223:208] + 1;
-            //     else if ((data_raw_reg[223:208] - 1) == 0) data_raw_reg[223:208] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[223:208] = data_raw_reg[223:208] - 1;
-            // end
+            if (sw[0] == 1 && sw[6] == 1) begin
+                if (data_raw_reg[223:208] == 0) data_raw_reg[223:208] = 16'd0000;
+                else data_raw_reg[223:208] = data_raw_reg[223:208] - 1;
+            end
 
-            // if (sw[0] == 1 && sw[7] == 1) begin
-            //     if (add_button) data_raw_reg[207:192] = data_raw_reg[207:192] + 1;
-            //     else if ((data_raw_reg[207:192] - 1) == 0) data_raw_reg[207:192] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[207:192] = data_raw_reg[207:192] - 1;
-            // end
+            if (sw[0] == 1 && sw[7] == 1) begin
+                if (data_raw_reg[207:192] == 0) data_raw_reg[207:192] = 16'd0000;
+                else data_raw_reg[207:192] = data_raw_reg[207:192] - 1;
+            end
 
-            // if (sw[1] == 1 && sw[4] == 1) begin
-            //     if (add_button) data_raw_reg[191:176] = data_raw_reg[191:176] + 1;
-            //     else if ((data_raw_reg[191:176] - 1) == 0) data_raw_reg[191:176] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[191:176] = data_raw_reg[191:176] - 1;
-            // end
+            if (sw[1] == 1 && sw[4] == 1) begin
+                if (data_raw_reg[191:176] == 0) data_raw_reg[191:176] = 16'd0000;
+                else data_raw_reg[191:176] = data_raw_reg[191:176] - 1;
+            end
 
-            // if (sw[1] == 1 && sw[5] == 1) begin
-            //     if (add_button) data_raw_reg[175:160] = data_raw_reg[175:160] + 1;
-            //     else if ((data_raw_reg[175:160] - 1) == 0) data_raw_reg[175:160] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[175:160] = data_raw_reg[175:160] - 1;
-            // end
+            if (sw[1] == 1 && sw[5] == 1) begin
+                if (data_raw_reg[175:160] == 0) data_raw_reg[175:160] = 16'd0000;
+                else data_raw_reg[175:160] = data_raw_reg[175:160] - 1;
+            end
 
-            // if (sw[1] == 1 && sw[6] == 1) begin
-            //     if (add_button) data_raw_reg[159:144] = data_raw_reg[159:144] + 1;
-            //     else if ((data_raw_reg[159:144] - 1) == 0) data_raw_reg[159:144] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[159:144] = data_raw_reg[159:144] - 1;
-            // end
+            if (sw[1] == 1 && sw[6] == 1) begin
+                if (data_raw_reg[159:144] == 0) data_raw_reg[159:144] = 16'd0000;
+                else data_raw_reg[159:144] = data_raw_reg[159:144] - 1;
+            end
 
-            // if (sw[1] == 1 && sw[7] == 1) begin
-            //     if (add_button) data_raw_reg[143:128] = data_raw_reg[143:128] + 1;
-            //     else if ((data_raw_reg[143:128] - 1) == 0) data_raw_reg[143:128] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[143:128] = data_raw_reg[143:128] - 1;
-            // end
+            if (sw[1] == 1 && sw[7] == 1) begin
+                if (data_raw_reg[143:128] == 0) data_raw_reg[143:128] = 16'd0000;
+                else data_raw_reg[143:128] = data_raw_reg[143:128] - 1;
+            end
 
-            // if (sw[2] == 1 && sw[4] == 1) begin
-            //     if (add_button) data_raw_reg[127:112] = data_raw_reg[127:112] + 1;
-            //     else if ((data_raw_reg[127:112] - 1) == 0) data_raw_reg[127:112] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[127:112] = data_raw_reg[127:112] - 1;
-            // end
+            if (sw[2] == 1 && sw[4] == 1) begin
+                if (data_raw_reg[127:112] == 0) data_raw_reg[127:112] = 16'd0000;
+                else data_raw_reg[127:112] = data_raw_reg[127:112] - 1;
+            end
 
-            // if (sw[2] == 1 && sw[5] == 1) begin
-            //     if (add_button) data_raw_reg[111:96] = data_raw_reg[111:96] + 1;
-            //     else if ((data_raw_reg[111:96] - 1) == 0) data_raw_reg[111:96] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[111:96] = data_raw_reg[111:96] - 1;
-            // end
+            if (sw[2] == 1 && sw[5] == 1) begin
+                if (data_raw_reg[111:96] == 0) data_raw_reg[111:96] = 16'd0000;
+                else data_raw_reg[111:96] = data_raw_reg[111:96] - 1;
+            end
 
-            // if (sw[2] == 1 && sw[6] == 1) begin
-            //     if (add_button) data_raw_reg[95:80] = data_raw_reg[95:80] + 1;
-            //     else if ((data_raw_reg[95:80] - 1) == 0) data_raw_reg[95:80] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[95:80] = data_raw_reg[95:80] - 1;
-            // end
+            if (sw[2] == 1 && sw[6] == 1) begin
+                if (data_raw_reg[95:80] == 0) data_raw_reg[95:80] = 16'd0000;
+                else data_raw_reg[95:80] = data_raw_reg[95:80] - 1;
+            end
 
-            // if (sw[2] == 1 && sw[7] == 1) begin
-            //     if (add_button) data_raw_reg[79:64] = data_raw_reg[79:64] + 1;
-            //     else if ((data_raw_reg[79:64] - 1) == 0) data_raw_reg[79:64] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[79:64] = data_raw_reg[79:64] - 1;
-            // end
+            if (sw[2] == 1 && sw[7] == 1) begin
+                if (data_raw_reg[79:64] == 0) data_raw_reg[79:64] = 16'd0000;
+                else data_raw_reg[79:64] = data_raw_reg[79:64] - 1;
+            end
 
-            // if (sw[3] == 1 && sw[4] == 1) begin
-            //     if (add_button) data_raw_reg[63:48] = data_raw_reg[63:48] + 1;
-            //     else if ((data_raw_reg[63:48] - 1) == 0) data_raw_reg[63:48] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[63:48] = data_raw_reg[63:48] - 1;
-            // end
+            if (sw[3] == 1 && sw[4] == 1) begin
+                if (data_raw_reg[63:48] == 0) data_raw_reg[63:48] = 16'd0000;
+                else data_raw_reg[63:48] = data_raw_reg[63:48] - 1;
+            end
 
-            // if (sw[3] == 1 && sw[5] == 1) begin
-            //     if (add_button) data_raw_reg[47:32] = data_raw_reg[47:32] + 1;
-            //     else if ((data_raw_reg[47:32] - 1) == 0) data_raw_reg[47:32] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[47:32] = data_raw_reg[47:32] - 1;
-            // end
+            if (sw[3] == 1 && sw[5] == 1) begin
+                if (data_raw_reg[47:32] == 0) data_raw_reg[47:32] = 16'd0000;
+                else data_raw_reg[47:32] = data_raw_reg[47:32] - 1;
+            end
 
-            // if (sw[3] == 1 && sw[6] == 1) begin
-            //     if (add_button) data_raw_reg[31:16] = data_raw_reg[31:16] + 1;
-            //     else if ((data_raw_reg[31:16] - 1) == 0) data_raw_reg[31:16] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[31:16] = data_raw_reg[31:16] - 1;
-            // end
+            if (sw[3] == 1 && sw[6] == 1) begin
+                if (data_raw_reg[31:16] == 0) data_raw_reg[31:16] = 16'd0000;
+                else data_raw_reg[31:16] = data_raw_reg[31:16] - 1;
+            end
 
-            // if (sw[3] == 1 && sw[7] == 1) begin
-            //     if (add_button) data_raw_reg[15:0] = data_raw_reg[15:0] + 1;
-            //     else if ((data_raw_reg[15:0] - 1) == 0) data_raw_reg[15:0] = 16'd0000;
-            //     else if (minus_button) data_raw_reg[15:0] = data_raw_reg[15:0] - 1;
-            // end
-
+            if (sw[3] == 1 && sw[7] == 1) begin
+                if (data_raw_reg[15:0] == 0) data_raw_reg[15:0] = 16'd0000;
+                else data_raw_reg[15:0] = data_raw_reg[15:0] - 1;
+            end
         end
     end
 
